7ad9d264309fddaf6fa3edd8d6cbc66d484221c1
Michael Schonfeld <michael@schonfeld.org>
2012-05-05 16:34:16 -0400

    coming along now

---
+++ example.php
@@ -0,0 +1,62 @@
+<?php
+// Include the Dwolla REST Client
+require 'dwolla.php';
+
+// CONSTANTS
+$apiKey = 'NrGOvb6djfAR9Pb2U1Jho+f+fuPRSuEUTfCiiJevNH2K/u4NQg';
+$apiSecret = 'PIJI6kXaRmbBVi2sgfRSdgqEjioIIbwOiMC+UvTQd/Oy5cWbU7';
+$token = 'LgMbBCjFXIlK2DuCF48gpVHQrkec+9Bn8Bo1EmxhkGkfwNzlaK';
+
+// Instantiate a new Dwolla REST Client
+$Dwolla = new DwollaRestClient($apiKey, $apiSecret, $token);
+
+
+// Get user's balance
+$balance = $Dwolla->balance();
+if(!$balance) { echo "Error: {$Dwolla->getError()} \n"; }
+echo "Your balance is: {$balance} \n";
+echo "-------------------- \n";
+
+// Send a money request
+$tid = $Dwolla->send('6443', '812-546-3855', 0.01, 'New PHP Library Test');
+if(!$tid) { echo "Error: {$Dwolla->getError()} \n"; }
+echo "Send transaction ID: {$tid} \n";
+echo "-------------------- \n";
+
+// Get transaction details
+$details = $Dwolla->transaction('743160');
+if(!$details) { echo "Error: {$Dwolla->getError()} \n"; }
+echo "Transaction details: {$details} \n";
+echo "-------------------- \n";
+
+// Get own account info
+$me = $Dwolla->me();
+if(!$me) { echo "Error: {$Dwolla->getError()} \n"; }
+echo "Own account: \n";
+print_r($me);
+echo "-------------------- \n";
+
+// Get user account info
+$user = $Dwolla->getUser('812-546-3855');
+if(!$user) { echo "Error: {$Dwolla->getError()} \n"; }
+echo "Own account: \n";
+print_r($user);
+echo "-------------------- \n";
+
+// Get contacts
+$contacts = $Dwolla->contacts('Ben');
+if(!$contacts) { echo "Error: {$Dwolla->getError()} \n"; }
+echo "Own contacts: \n";
+print_r($contacts);
+echo "-------------------- \n";
+
+// Get nearby contacts
+$nearbyContacts = $Dwolla->nearbyContacts('Ben');
+if(!$nearbyContacts) { echo "Error: {$Dwolla->getError()} \n"; }
+echo "Nearby contacts: \n";
+print_r($nearbyContacts);
+echo "-------------------- \n";
+
+
+
+
